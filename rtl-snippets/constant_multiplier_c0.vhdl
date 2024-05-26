cd1c3d159169def3a02c8fe830b716f8ff27bda0
gustavkol <gustavko@stud.ntnu.no>
2024-05-26 12:31:48 +0200

    removing excess files

--- rtl-snippets/constant_multiplier_c0.vhdl
+++
@@ -1,3 +0,0 @@
--- (2*p*(f_s/v_s)^2) * 10^-3 = 131.767 = 0b0100000111100
-op3      <= std_logic_vector(unsigned(shift_left(unsigned(op1),7+4)) + unsigned(shift_left(unsigned(op1),1+4)) + unsigned(shift_left(unsigned(op1),0+4))
-                            + unsigned(shift_left(unsigned(op1),4-1)) + unsigned(shift_left(unsigned(op1),4-2)));
